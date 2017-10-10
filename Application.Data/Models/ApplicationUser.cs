8fafeb37a03921518bd02144ee69bacd7a6978ec
vitalicok <vitalijshal@gmail.com>
2017-10-10 14:53:26 +0200

    Adding new project Data for managing and saving data EF. Using Repository Pattern.

---
+++ Application.Data/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Data.Models
+{
+    class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(
+            UserManager<ApplicationUser> userManager, string authentificationType)
+        {
+            var userIdentity = await userManager.CreateIdentityAsync(this, authentificationType);
+
+            return userIdentity;
+        }
+    }
+}
---
+++ Application.Services/Models/User/RegisterUserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Services.Models
+{
+    public class RegisterUserModel
+    {
+    }
+}
