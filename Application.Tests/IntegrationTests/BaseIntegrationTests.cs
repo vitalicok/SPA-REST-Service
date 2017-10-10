3d6c31412f9508dfa6cfd2b8508007f6659cbe21
vitalicok <vitalijshal@gmail.com>
2017-10-10 14:58:43 +0200

    Adding new project Tests for testing all application.

---
+++ Application.Tests/IntegrationTests/BaseIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.IntegrationTests
+{
+    [TestClass]
+    public class BaseIntegrationTests
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+
+        }
+        
+        protected void ReloadContext()
+        {
+            
+        }
+    }
+}
---
+++ Application.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Application.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b238355a-b604-473f-8896-526ef442779e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
